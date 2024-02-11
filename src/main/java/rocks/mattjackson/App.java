ad8e92241f712e7d8da05e0e50cd1460b00b08d6
Matt J <mdjackson97@gmail.com>
2024-02-11 17:03:43 -0500

    Multi-threaded handling

--- src/main/java/rocks/mattjackson/App.java
+++ src/main/java/rocks/mattjackson/App.java
@@ -1,13 +1,8 @@
 package rocks.mattjackson;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import rocks.mattjackson.controllers.GenericController;
 
 /**
  * Hello world!
@@ -19,25 +14,15 @@ public class App
 	{
 		int portNumber = 8080;
 		System.out.println("Server starting. Available at localhost:"+portNumber);
+		ThreadedAction actor = new ThreadedAction(4);
 		try (
 			ServerSocket serverSocket = new ServerSocket(portNumber);
-			Socket socket = serverSocket.accept();
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		) {
-			
 			while(true) {
-				String inLine = in.readLine();
-				if (inLine == null || inLine.isEmpty()) {
-					break;
-				}
-				System.out.println("From client: "+(inLine == null ? "<< null >>" : inLine)+inLine.length());
+				Socket socket = serverSocket.accept();
+				actor.act(socket);
+				System.out.println("looping");
 			}
-			
-			String output = new GenericController().handle(null);
-			System.out.println("Done reading from client");
-			System.out.println("Returning the value "+output);
-			out.println(output);
 		} catch (IOException e) {
 			System.out.println("Encountered an error: "+e.getMessage());
 		}
---
+++ src/main/java/rocks/mattjackson/ThreadedAction.java
@@ -0,0 +1,58 @@
+package rocks.mattjackson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import rocks.mattjackson.controllers.GenericController;
+
+public class ThreadedAction {
+	
+	private Executor executor;
+	
+	public ThreadedAction(int threadPoolSize) {
+		executor = Executors.newFixedThreadPool(threadPoolSize);
+	}
+	
+	public void act(final Socket socket) {
+		executor.execute(() -> {
+			sendToController(socket);
+			closeSocket(socket);
+		});
+//		sendToController(socket);
+//		closeSocket(socket);
+	}
+	
+	private void sendToController(Socket socket) {
+		try (
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			) {
+				while(true) {
+					String inLine = in.readLine();
+					if (inLine == null || inLine.isEmpty()) {
+						break;
+					}
+					System.out.println("From client: "+(inLine == null ? "<< null >>" : inLine));
+				}
+				
+				String output = new GenericController().handle(null);
+				System.out.println("Done reading from client");
+				System.out.println("Returning the value "+output);
+				out.println(output);
+			} catch (IOException e) {
+				System.out.println("Unable to run ThreadedAction "+e);
+			}
+	}
+	
+	private void closeSocket(Socket socket) {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Unable to close socket "+e);
+		}
+	}
+}
